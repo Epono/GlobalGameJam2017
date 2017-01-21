25f256d179e09c29bf9e61701dca66422f796c02
LeRyokan <tixier.lucas@gmail.com>
2017-01-21 03:21:28 +0100

    LightHouse sprite + anim

---
+++ Assets/LightHouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightHouse : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.Rotate(0, 0, -20 * Time.deltaTime);
+    }
+}
