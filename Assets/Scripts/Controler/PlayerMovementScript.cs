d9197af0dc4945a9c65e48ccc1111382f2612914
VisualPi <jonathan.loquet@gmail.com>
2017-01-22 03:19:15 +0100

    Fix crash rocketCollision + serializeField dans prefab et cooldown du tir

--- Assets/Scripts/Controler/PlayerMovementScript.cs
+++ Assets/Scripts/Controler/PlayerMovementScript.cs
@@ -5,159 +5,172 @@ using UnityEngine;
 
 public class PlayerMovementScript : NetworkBehaviour
 {
-    [SerializeField]
-    private GameObject bulletPrefab;
+	[SerializeField]
+	private GameObject bulletPrefab;
 
-    [SerializeField]
-    private Transform bulletSpawn;
+	[SerializeField]
+	private Transform bulletSpawn;
 
-    [SerializeField]
-    private WarshipScript script;
+	[SerializeField]
+	private WarshipScript script;
 
-    [SerializeField]
-    private ScanScript scanScript;
+	[SerializeField]
+	private ScanScript scanScript;
 
-    private WarshipAttributes attributes;
+	private WarshipAttributes attributes;
 
-    [SyncVar]
-    private int currentHealth;
+	[SyncVar]
+	private int currentHealth;
 
-    private Vector2 forward = new Vector2(0,1);
-    private Vector2 aim;
-    private bool shootFire = false;
-    private bool shootSonar = false;
+	private Vector2 forward = new Vector2(0,1);
+	private Vector2 aim;
+	private bool shootFire = false;
+	private bool shootSonar = false;
+	private bool _canFire = true;
 
-    float  x = 0.0f;
-    public float MovementMagicNumber = 0.05f; // value for Input.GetAxis("Vertical") * Time.deltaTime * 3.0f; needed hard coded
-    
-    void Start()
-    {
-        attributes = script.Attributes;
-        currentHealth = attributes.HealthPoint;
-    }
 
-    public void readInfo(InfoSend infos)
-    {
-        x = infos.move;
-        aim = infos.aimAngle;
-        shootFire  = infos.inputListing[Action.FIRE];
-        shootSonar = infos.inputListing[Action.WAVESHOT];
-    }
+	float  x = 0.0f;
+	public float MovementMagicNumber = 0.05f; // value for Input.GetAxis("Vertical") * Time.deltaTime * 3.0f; needed hard coded
+	
+	void Start()
+	{
+		attributes = script.Attributes;
+		currentHealth = attributes.HealthPoint;
+	}
 
-    void Update()
-    {
-        //transform.Translate(0, MovementMagicNumber, 0);// attributes.MoveSpeed * Time.deltaTime * 3.0f, 0);
-        if (!isLocalPlayer)
-        {
-            return;
-        }
+	public void readInfo(InfoSend infos)
+	{
+		x = infos.move;
+		aim = infos.aimAngle;
+		shootFire  = infos.inputListing[Action.FIRE];
+		shootSonar = infos.inputListing[Action.WAVESHOT];
+	}
 
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+	void Update()
+	{
+		//transform.Translate(0, MovementMagicNumber, 0);// attributes.MoveSpeed * Time.deltaTime * 3.0f, 0);
+		if (!isLocalPlayer)
+		{
+			return;
+		}
 
-        
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        transform.Rotate(0, 0, -x);
-        transform.Translate(0, y, 0);
-        
+		
 
-        if(aim.magnitude >= 0.5f)
-        {
-            forward = aim.normalized;
-            forward.y *= -1;
-            bulletSpawn.position = transform.position;
-            bulletSpawn.position += new Vector3(forward.x, forward.y, 0)*0.5f;
-        }
-        
+		transform.Rotate(0, 0, -x);
+		transform.Translate(0, y, 0);
+		
 
-        if (shootFire)
-        {
-            if (attributes.Ammunition > 0)
-            {
-                CmdFire(forward);
-                attributes.Ammunition--;
-            }
-        }
+		if(aim.magnitude >= 0.5f)
+		{
+			forward = aim.normalized;
+			forward.y *= -1;
+			bulletSpawn.position = transform.position;
+			bulletSpawn.position += new Vector3(forward.x, forward.y, 0)*0.5f;
+		}
+		
 
-        if(shootSonar)
-        {
-            //gestion coolDown
-            scanScript.RunScan();
-        }
-    }
+		if (shootFire)
+		{
+			if( _canFire )
+			{
+				_canFire = false;
+				if( attributes.Ammunition > 0 )
+				{
+					CmdFire(forward);
+					attributes.Ammunition--;
+				}
+				StartCoroutine("OnBulletFired");
+			}
+		}
 
-    // This [Command] code is called on the Client …
-    // … but it is run on the Server!
-    [Command]
-    void CmdFire(Vector2 forward)
-    {
-        // Create the Bullet from the Bullet Prefab
-        var bullet = (GameObject)Instantiate(
-            bulletPrefab,
-            bulletSpawn.position,
-            bulletSpawn.rotation);
-        
-        // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody2D>().velocity = forward * 6;
+		if(shootSonar)
+		{
+			//gestion coolDown
+			scanScript.RunScan();
+		}
+	}
 
-        // Spawn the bullet on the Clients
-        NetworkServer.Spawn(bullet);
+	// This [Command] code is called on the Client …
+	// … but it is run on the Server!
+	[Command]
+	void CmdFire(Vector2 forward)
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+		
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody2D>().velocity = forward * 6;
 
-        // Destroy the bullet after 2 seconds
-        Destroy(bullet, 2.0f);
-    }
+		// Spawn the bullet on the Clients
+		NetworkServer.Spawn(bullet);
 
-    public override void OnStartLocalPlayer()
-    {
-        //GetComponent<MeshRenderer>().material.color = Color.blue;
-    }
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
+	}
 
-    public void TakeDamage(int amount)
-    {
+	public override void OnStartLocalPlayer()
+	{
+		//GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
 
-        if (!isServer)
-        {
-            return;
-        }
-        currentHealth -= amount;
-        attributes.HealthPoint -= currentHealth;
-        Debug.Log(currentHealth);
-        if (currentHealth <= 0)
-        {
-            currentHealth = 0;
-            Debug.Log("Dead!");
-            //LUCAS BOOLEAN ICI
-        }
-    }
+	public void TakeDamage(int amount)
+	{
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
+		if (!isServer)
+		{
+			return;
+		}
+		currentHealth -= amount;
+		attributes.HealthPoint -= currentHealth;
+		Debug.Log(currentHealth);
+		if (currentHealth <= 0)
+		{
+			currentHealth = 0;
+			Debug.Log("Dead!");
+			//LUCAS BOOLEAN ICI
+		}
+	}
 
-        var hit = collision.gameObject;
-        if (hit.tag == "WARSHIP")
-        {
-            var health = hit.GetComponent<PlayerMovementScript>();
-            if (health != null)
-            {
-                health.TakeDamage(attributes.CollisionDamage);
-                TakeDamage(health.attributes.CollisionDamage);
-            }
-        }
-        else if (hit.tag == "ISLAND")
-        {
-            var health = hit.GetComponent<IsleAttribute>();
-            if (health != null)
-            {
-                health.HealthPoint--;
-                TakeDamage(attributes.CollisionDamage);
-                if (health.HealthPoint <= 0)
-                {
-                    Destroy(hit);
-                    //change sprit of islands
-                }
-            }
-        }
+	void OnCollisionEnter2D(Collision2D collision)
+	{
 
-        Destroy(gameObject);
-    }
+		var hit = collision.gameObject;
+		if (hit.tag == "WARSHIP")
+		{
+			var health = hit.GetComponent<PlayerMovementScript>();
+			if (health != null)
+			{
+				health.TakeDamage(attributes.CollisionDamage);
+				TakeDamage(health.attributes.CollisionDamage);
+			}
+		}
+		else if (hit.tag == "ISLAND")
+		{
+			var health = hit.GetComponent<IsleAttribute>();
+			if (health != null)
+			{
+				health.HealthPoint--;
+				TakeDamage(attributes.CollisionDamage);
+				if (health.HealthPoint <= 0)
+				{
+					Destroy(hit);
+					//change sprit of islands
+				}
+			}
+		}
+
+		Destroy(gameObject);
+	}
+	public IEnumerator OnBulletFired()
+	{
+		yield return new WaitForSeconds(0.5f);
+		_canFire = true;
+		yield return null;
+	}
 }
--- Assets/Scripts/Controler/RocketCollisionScript.cs
+++ Assets/Scripts/Controler/RocketCollisionScript.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class RocketCollisionScript : MonoBehaviour {
 
-    [SerializeField]
-    private RocketScript rocketAttributes;
+	[SerializeField]
+	private RocketScript rocketAttributes;
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
+	void OnCollisionEnter2D(Collision2D collision)
+	{
 
-        var hit = collision.gameObject;
-        if(hit.tag == "WARSHIP")
-        {
-            var health = hit.GetComponent<PlayerMovementScript>();
-            if (health != null)
-            {
-                health.TakeDamage(rocketAttributes.Damages);
-            }
-        }
-        else if(hit.tag == "ISLAND")
-        {
-            var health = hit.GetComponent<IsleAttribute>();
-            if (health != null)
-            {
-                health.HealthPoint--;
-              
-                hit.GetComponent<SpriteRenderer>().sprite = health.spriteList[health.HealthPoint - 1];
-                if (health.HealthPoint <=0 )
-                {
-                    Destroy(hit);
-                }
-            }
-        }
+		var hit = collision.gameObject;
+		if(hit.tag == "WARSHIP")
+		{
+			var health = hit.GetComponent<PlayerMovementScript>();
+			if (health != null)
+			{
+				health.TakeDamage(rocketAttributes.Damages);
+			}
+		}
+		else if(hit.tag == "ISLAND")
+		{
+			var health = hit.GetComponent<IsleAttribute>();
+			if (health != null)
+			{
+				health.HealthPoint--;
+				if ((health.HealthPoint-1) >= 0)
+					hit.GetComponent<SpriteRenderer>().sprite = health.spriteList[health.HealthPoint - 1];
+				else if (health.HealthPoint <=0 )
+				{
+					Destroy(hit);
+				}
+			}
+		}
 
-        Destroy(gameObject);
-    }
+		Destroy(gameObject);
+	}
 }
